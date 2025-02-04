33e6016590d7abb6d3baaa5d1518d40c91f1505d
Samuglc <166132928+Samuglc@users.noreply.github.com>
2025-02-04 19:13:18 -0400

    Add files via upload

---
+++ Samuel Proyecto/ProductoOProveedor/ProductoOProveedor.vue
@@ -0,0 +1,32 @@
+<template src="../views/ProductoOProveedor.html"></template>
+
+
+<script>
+import "../assets/ProductoOProveedor.css";
+export default {
+    data() {
+        return {
+            searchQuery: "",
+            selectedCategoria: "",
+            productos: [
+                { fecha: '2024-02-01', proveedor: 'Proveedor A', nombre: 'Laptop ASUS', categoria: 'Electrónicos', costo: '$800' },
+                { fecha: '2024-02-05', proveedor: 'Proveedor B', nombre: 'Camiseta Nike', categoria: 'Ropa', costo: '$50' },
+                { fecha: '2024-02-10', proveedor: 'Proveedor C', nombre: 'Smartphone Samsung', categoria: 'Electrónicos', costo: '$900' },
+                { fecha: '2024-02-15', proveedor: 'Proveedor D', nombre: 'Pan Bimbo', categoria: 'Alimentos', costo: '$3' }
+            ]
+        };
+    },
+    computed: {
+        filteredProductos() {
+            let filtered = this.productos.filter(producto =>
+                producto.nombre.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+                producto.proveedor.toLowerCase().includes(this.searchQuery.toLowerCase())
+            );
+            if (this.selectedCategoria) {
+                filtered = filtered.filter(p => p.categoria === this.selectedCategoria);
+            }
+            return filtered;
+        }
+    }
+};
+</script>
---
+++ Samuel Proyecto/app.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <InventarioTable />
+      <MovimientoInventarioTable />
+      <ProductoOProveedor />
+      <ProductoTable />
+      <ProveedorTable />
+      <ProyeccionConsumoTable />
+    </div>
+  </template>
+  
+  <script>
+  import InventarioTable from './components/tables/InventarioTable.vue';
+  import MovimientoInventarioTable from './components/tables/MovimientoInventarioTable.vue';
+  import ProductoOProveedor from './components/tables/ProductoOProveedor.vue';
+  import ProductoTable from './components/tables/ProductoTable.vue';
+  import ProveedorTable from './components/tables/ProveedorTable.vue';
+  import ProyeccionConsumoTable from './components/tables/ProyeccionConsumoTable.vue';
+  
+  export default {
+    components: {
+      InventarioTable,
+      MovimientoInventarioTable,
+      ProductoOProveedor,
+      ProductoTable,
+      ProveedorTable,
+      ProyeccionConsumoTable
+    }
+  };
+  </script>
+  
